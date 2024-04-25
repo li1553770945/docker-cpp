1a8e4fec5a7d6458eae585690d1b1965aed5963e
li1553770945 <1553770945@qq.com>
2024-04-26 00:28:56 +0800

    bugfix(child):修复不能删除旧root的bug

--- child/child.cpp
+++ child/child.cpp
@@ -12,11 +12,14 @@
 Child::Child(const Config &config, const Logger &log) : log(log),
                                                         config(config)
 {
+    this->Init();
 }
 void Child::Run()
 {
-    this->Init();
-    execve(config.command.data(),NULL, NULL);
+    const char *args[] = {"/bin/sh",  nullptr};
+    log[Info]<<"running..."<<endl;
+    execve("/bin/sh",const_cast<char* const*>(args), NULL);
+    log[Info]<<"running end"<<endl;
 }
 
 void Child::Init()
@@ -30,20 +33,43 @@ void Child::InitHostname()
 }
 void Child::InitMount()
 {
-    mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL);
-
-    string mount_dir = "/tmp/"+config.hostname;
+    int res;
+    res = mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL);
+    if (res)
+    {
+        log[Fatal] << "set / to MS_PRIVATE FAILED! " << strerror(errno) << endl;
+    }
+    string mount_dir = "/tmp/" + config.hostname + "XXXXXX";
     mkdtemp(mount_dir.data());
-    mount(config.mount_dir.data(), mount_dir.data(), NULL, MS_BIND | MS_PRIVATE, NULL);
-    log[Info]<<"mount dir "<<mount_dir<<endl;
-    string old_mount_basename ="/oldroot."+config.hostname;
+    res = mount(config.mount_dir.data(), mount_dir.data(), NULL, MS_BIND | MS_PRIVATE, NULL);
+    if (res)
+    {
+        log[Fatal] << "mount mount dir FAILED! " << strerror(errno) << endl;
+    }
+    log[Info] << "mount dir " << mount_dir << endl;
+
+
+
+    string old_mount_basename = "/oldroot." + config.hostname + "XXXXXX";
     string old_mount_dir = mount_dir + old_mount_basename;
-    mkdtemp(old_mount_dir.data());
-    log[Info]<<"old mount dir"<<old_mount_dir<<endl;
-    syscall(SYS_pivot_root, mount_dir.data(), old_mount_dir.data());
+    old_mount_dir = mkdtemp(old_mount_dir.data());
+    old_mount_basename = basename(old_mount_dir.data());
 
-
-    chdir("/"); //取消挂载原来的根目录。因为旧的目录就挂载在新的根目录下面，因此先切换到新的根目录
-    umount2(old_mount_basename.data(), MNT_DETACH);
-    rmdir(old_mount_basename.data());
+    log[Info] << "old mount dir" << old_mount_dir << endl;
+    res = syscall(SYS_pivot_root, mount_dir.data(), old_mount_dir.data());
+    if (res<0)
+    {
+        log[Fatal] << "pivot_root syscall FAILED! " << strerror(errno) << endl;
+    }
+    chdir("/"); // 取消挂载原来的根目录。因为旧的目录就挂载在新的根目录下面，因此先切换到新的根目录
+    res = umount2(old_mount_basename.data(), MNT_DETACH);
+    if (res)
+    {
+        log[Error] << "unmount old root failed! " << strerror(errno) << endl;
+    }
+    res = rmdir(old_mount_basename.data());
+      if (res)
+    {
+        log[Error] << "delete old root failed! " << strerror(errno) << endl;
+    }
 }
